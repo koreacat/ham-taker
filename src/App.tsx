15e63b09d135c3afeded13a9522fe2e3587ed589
지경민 <kj9966@naver.com>
2021-07-08 14:16:45 +0900

    'init'

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HamTaker from "./components/Hamtaker";
+
+function App() {
+	return (
+		<div className="App">
+			<HamTaker/>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Hamtaker.tsx
@@ -0,0 +1,9 @@
+const HamTaker = () => {
+	return (
+		<div>
+			hamtaker
+		</div>
+	)
+};
+
+export default HamTaker;
